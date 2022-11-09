430e06ee80e7883229770a7df7ff701cd4354d3d
JIA-BIN-CHANG <alvin71134@gmail.com>
2022-11-09 14:50:59 -0500

    working on hw16

--- hw16/IR19B.py
+++ hw16/IR19B.py
@@ -9,6 +9,7 @@ import random
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, normalized_mutual_info_score, rand_score
+from sklearn.metrics.cluster import contingency_matrix
 import matplotlib.pyplot as plt
 
 f = open("bible.txt", "r")
@@ -38,3 +39,17 @@ cm2 = cm * 2 # Exercise 16.3: replace every point d with two identical copies
 # You code to compute and print NMI, RI, and purity(in two ways)
 # see slide 11/3/15 for purity calculation
 # Then run the same on cm2 so you get the answer to Exercise 16.3 (ii)
+
+nmi_score = normalized_mutual_info_score(y1,y2)
+ri_score = rand_score(y1,y2)
+
+a0 = np.sum(np.amax(cm, axis=0))
+a1 = np.sum(np.amax(cm, axis=1))
+n = np.sum(cm)
+purity_score = a0/n
+transpose_purity_score = a1/n
+
+print(f'NMI score: {nmi_score}')
+print(f'Rand index score: {ri_score}')
+print(f'purity score: {purity_score}')
+print(f'pruity score on transpose matrix: {transpose_purity_score}')
