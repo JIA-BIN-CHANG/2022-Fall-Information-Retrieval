423e26a1056e4c6761572dc13de1ab1fa7805ea5
JIA-BIN-CHANG <alvin71134@gmail.com>
2022-11-13 15:50:50 -0500

    hw17 code done

--- hw17/IR20A.py
+++ hw17/IR20A.py
@@ -6,9 +6,9 @@
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import AgglomerativeClustering
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, 
-normalized_mutual_info_score
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, normalized_mutual_info_score
 import matplotlib.pyplot as plt
+import itertools as it
 
 f = open("bible.txt", "r")
 docs = f.readlines()
@@ -25,8 +25,23 @@ single.fit_predict(X)
 complete = AgglomerativeClustering(n_clusters=4, linkage='complete')
 complete.fit_predict(X)
 
-cm = confusion_matrix(single.labels_, complete.labels_)
+average = AgglomerativeClustering(n_clusters=4, linkage='average')
+average.fit_predict(X)
 
-disp = ConfusionMatrixDisplay(cm)
-disp.plot()
+ward = AgglomerativeClustering(n_clusters=4, linkage='ward')
+ward.fit_predict(X)
+
+sets = [single, complete, ward, average]
+names = ['single', 'complete', 'ward', 'average']
+sets_combinations = it.combinations(sets, 2)
+sets_combinations = list(sets_combinations)
+names_combinations = it.combinations(names, 2)
+names_combinations = list(names_combinations)
+
+for set, name in zip(sets_combinations, names_combinations):
+    cm = confusion_matrix(set[0].labels_, set[1].labels_)
+    disp = ConfusionMatrixDisplay(cm)
+    disp.plot()
+    nmi_score = normalized_mutual_info_score(set[0].labels_, set[1].labels_)
+    print(f'NMI score between {name[0]} and {name[1]} is: {nmi_score}')
 plt.show()
